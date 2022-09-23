f36b54fc95677c62a899981348e78b6121cc9528
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-23 17:40:15 -0300

    feat: adds user creation on fauna as callback to sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,8 +16,17 @@ export default NextAuth({
     // add more authentication providers as necessary
   ],
   callbacks: {
-    session({ session }) {
-      return session;
+    async signIn({ user }) {
+      const { email } = user;
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        )
+      );
+
+      return true;
     },
   }
 });
