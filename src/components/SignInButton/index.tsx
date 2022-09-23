decab7a9b0ae746ea60f1e665acc3346bdda8156
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-23 17:07:34 -0300

    feat: updates sign in btn component to useSession

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,23 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Julia F
+      { session.user.name }
       <FiX color="#737380" />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
