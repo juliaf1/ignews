be298df619e74546b5598dd54e42bf16b8172a3c
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-23 17:06:49 -0300

    feat: adds session provider context to myapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,26 @@
 import { AppProps } from 'next/app';
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 
 import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  pageProps: {
+    session: Session;
+  }
+}
+
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps }
+}: MyAppProps) {
   return(
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
