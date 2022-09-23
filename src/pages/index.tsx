3e75f64d38cff29b8c2ee4c5b97ace4f4ec139ac
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-22 22:22:53 -0300

    feat: adds home banner layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,17 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
+
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all publications<br/>
+            <span>for $9.9/month</span>
+          </p>
+        </section>
+      </main>
     </>
   )
 }
