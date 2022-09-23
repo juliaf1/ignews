7ec702e175b801f29683bbd8a09131e8d3c90437
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-22 22:17:28 -0300

    feat: adds structure for logged in user button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,11 +1,20 @@
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  return(
-    <button>
-      <FaGithub />
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Julia F
+      <FiX color="#737380" />
+    </button>
+  ) : (
+    <button className={styles.signInButton}>
+      <FaGithub color="#eba417" />
       Sign in with Github
     </button>
   )
