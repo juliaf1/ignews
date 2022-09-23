53e11e9f8f116b0446d632b171fa2189f8b39d59
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-23 20:37:27 -0300

    feat: add fauna if query to persist user only if it doesnt exist

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,16 +24,31 @@ export default NextAuth({
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         );
 
         return true;
       } catch (error) {
-        const { description } = error;
-        return description.includes('is not unique');
+        return false;
       };
     },
   }
