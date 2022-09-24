7ced6fd3f03672a41c3c81b2ff0b53811f1c9a2c
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-23 21:25:25 -0300

    feat: save stripe customer id as user field on db

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,15 +1,44 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react';
+import { query as q } from 'faunadb';
 
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
+
+interface User {
+  ref: {
+    id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req });
+    const { email } = session.user;
 
     const customer = await stripe.customers.create({
-      email: session.user.email,
+      email
     });
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(email)
+        )
+      )
+    )
+
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: {
+            stripe_customer_id: customer.id
+          }
+        }
+      )
+    )
 
     const checkoutSession = await stripe.checkout.sessions.create({
       customer: customer.id,
