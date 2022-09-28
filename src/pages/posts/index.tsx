923f3b446c8ea9945be460e98f39c6aa5e396092
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-28 19:05:58 -0300

    feat: adds get static props to posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+interface Post {
+  id: string;
+  url: string;
+  title: string;
+  excerpt: string;
+  created_at: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -11,25 +24,27 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março, 2022</time>
-            <h3>Creating a Next application with TS</h3>
-            <p>In this guide, you will learn how to create a Next application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.</p>
-          </a>
-
-          <a>
-            <time>12 de março, 2022</time>
-            <h3>Creating a Next application with TS</h3>
-            <p>In this guide, you will learn how to create a Next application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.</p>
-          </a>
-
-          <a>
-            <time>12 de março, 2022</time>
-            <h3>Creating a Next application with TS</h3>
-            <p>In this guide, you will learn how to create a Next application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.</p>
-          </a>
+          { posts.map(post => (
+              <a href={post.url} key={post.id}>
+                <time>{post.created_at}</time>
+                <h3>{post.title}</h3>
+                <p>{post.excerpt}</p>
+              </a>
+          )) }
         </div>
       </main>
     </>
   )
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      posts: [
+        { id: '1', url: 'https://google.com', title: 'Creating a Next application with TS', excerpt: 'In this guide, you will learn how to create a Next application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.', created_at: 'March 22, 2022'},
+        { id: '2', url: 'https://google.com', title: 'Creating a Next application with TS', excerpt: 'In this guide, you will learn how to create a Next application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.', created_at: 'March 22, 2022'},
+        { id: '3', url: 'https://google.com', title: 'Creating a Next application with TS', excerpt: 'In this guide, you will learn how to create a Next application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.', created_at: 'March 22, 2022'},
+      ]
+    }
+  }
 };
