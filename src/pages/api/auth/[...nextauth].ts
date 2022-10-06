3376226d035362c11043e32fee12d66638afd1bf
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-06 14:04:56 -0300

    feat: overrides session callback to include active subscription data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,6 +19,42 @@ export default NextAuth({
     secret: process.env.SIGNIN_KEY,
   },
   callbacks: {
+    async session({ session, user }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Query(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                'active'
+              )
+            ])
+          )
+        );
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      };
+    },
     async signIn({ user }) {
       const { email } = user;
 
